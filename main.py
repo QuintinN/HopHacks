e334692eee378d7860053f552427fa7ab0dfc269
naveenr414 <dsfan414@gmail.com>
2019-02-16 01:15:49 -0500

    Merged twilio into main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
 import sqlite3
-from datetime import datetime 
+from datetime import datetime
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 database_name = "database.db" 
@@ -27,7 +28,13 @@ def delete_all(ID):
     c = conn.cursor()
     c.execute("DELETE from messages where ID=?",(ID,))
     conn.commit()
-    conn.close() 
+    conn.close()
+
+@app.route("/sms", methods = ['GET', 'POST'])
+def sms_reply():
+    resp = MessagingResponse()
+    resp.message("The Robots are coming! Head for the hills!")
+    return str(resp)
 
 def select_all(ID):
     conn = sqlite3.connect(database_name)
